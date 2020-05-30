156c2ea0f047149e085b5f9c5b0504268764219b
老秦 <50172189@qq.com>
2020-05-30 21:30:02 +0800

    Create ConfigProvider.php

---
+++ src/ConfigProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  50172189@qq.com
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Ericjank\Htcc;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'publish' => [
+                [
+                    'id' => 'config',
+                    'description' => 'The config for aliyun tablestore.',
+                    'source' => __DIR__ . '/../publish/tablestore.php',
+                    'destination' => BASE_PATH . '/config/autoload/tablestore.php',
+                ],
+            ],
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+            ],
+        ];
+    }
+}
